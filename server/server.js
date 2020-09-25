9748aa516e66d47f983b4552acf01e4ca9aa4451
Chance Nguyen <chanceknguyen@gmail.com>
2020-09-24 19:06:59 -0500

    Attempt to fix CircleCI issues

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3003;
 
 app.get('/', (req, res) => {
+
   res.send('Campsite-Host-Info Server Test');
+
 })
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
