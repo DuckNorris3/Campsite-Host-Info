62fde46fca46c9578659ae3d82d1569f4a781716
Chance Nguyen <chanceknguyen@gmail.com>
2020-09-25 22:05:30 -0500

    Create seeding database script

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,12 @@
 var mysql = require('mysql');
+var faker = require('faker');
+
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
 });
+
+
 
 connection.connect(function(err) {
   if (err) throw err;
@@ -16,11 +20,11 @@ connection.connect(function(err) {
     console.log('created database tenthopCampsiteInfo');
   });
   connection.query('USE tenthopCampsiteInfo');
-  connection.query(`CREATE TABLE host(id int NOT NULL AUTO_INCREMENT, hostName varchar(40), primary key(id) )`,(err, result) => {
+  connection.query(`CREATE TABLE host(id int NOT NULL AUTO_INCREMENT, hostName varchar(40), avatar varchar(240), primary key(id) )`,(err, result) => {
     if (err) throw err;
     console.log('table host created');
   });
-  connection.query(`CREATE TABLE guest(id int NOT NULL AUTO_INCREMENT, guestName varchar(40), primary key(id) )`,(err, result) => {
+  connection.query(`CREATE TABLE guest(id int NOT NULL AUTO_INCREMENT, guestName varchar(40), avatar varchar(240), primary key(id) )`,(err, result) => {
     if (err) throw err;
     console.log('table guest created');
   });
@@ -33,9 +37,37 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log('table guestRecommendSite created');
   });
+  var counter = 1;
+  for (let i = 0; i < 100; i++ ) {
+    var hostName = faker.name.findName();
+    var hostAvatar = faker.internet.avatar();
+    var guestName = faker.name.findName();
+    var guestAvatar = faker.internet.avatar();
+    var siteName = faker.company.companyName();
+    var description = faker.commerce.productDescription();
+    var hostId = faker.random.number(99);
+    var location = {country: 'United States', state: `${faker.address.state()}`};
+    var guestId = faker.random.number(99);
+    var siteId = faker.random.number(99);
+
+
+
+    connection.query(`INSERT INTO host (hostName, avatar) values ("${hostName}", "${hostAvatar}")`, (err, result) => {
+      if (err) throw err;
+    });
+    connection.query(`INSERT INTO guest (guestName, avatar) values ("${guestName}", "${guestAvatar}")`, (err, result) => {
+      if (err) throw err;
+    });
+    connection.query(`INSERT INTO site (siteName, hostId, location, description) values ("${siteName}", "${counter}", "${location}", "${description}")`, (err, result) => {
+      if (err) throw err;
+    });
+    connection.query(`INSERT INTO guestRecommendSite (guestId, siteId) values ("${counter}", "${counter}")`, (err, result) => {
+      if (err) throw err;
+    });
+    counter++;
+  }
 
   connection.end();
-
 })
 
 
