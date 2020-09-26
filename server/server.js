22b21f45ace6435a14e7410b1b3182c82fb7c446
Chance Nguyen <chanceknguyen@gmail.com>
2020-09-25 22:47:31 -0500

    Refactor server to handle database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.port || 3003;
+const db = require('../db/index.js');
 
 app.use(express.static('./client/dist'));
+
+app.get('/api/sites', (req, res) => {
+  console.log('receiving a get request');
+  debugger;
+  db.readSites((err, result) => {
+    if (err) throw err;
+    console.log('sending the results');
+    res.send(result);
+  })
+})
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
