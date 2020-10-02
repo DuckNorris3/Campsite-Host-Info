f8bbb176aaf150c9b5ba4e8b76b685873636f591
Chance Nguyen <chanceknguyen@gmail.com>
2020-10-02 17:13:26 -0500

    Remove wireframe

--- client/src/Description.jsx
+++ client/src/Description.jsx
@@ -7,7 +7,10 @@ import { DescriptionContainer, CovidGuidelines, DescriptionText } from './styles
 const Description = ({ site }) => (
   <DescriptionContainer>
     <CovidGuidelines>
-      Justin has self-certified that Hipcamp’s COVID-19 Safety Standards have been implemented at this listing. See what’s being done here.
+      <svg stroke="currentColor" fill="currentColor" strokeWidth="2" viewBox="0 -1 16 16" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M13.854 3.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3.5-3.5a.5.5 0 11.708-.708L6.5 10.293l6.646-6.647a.5.5 0 01.708 0z" clipRule="evenodd" /></svg>
+      <span>
+        Justin has self-certified that Hipcamp’s COVID-19 Safety Standards have been implemented at this listing. See what’s being done here.
+      </span>
     </CovidGuidelines>
     <DescriptionText>
       { site.description }
--- client/src/Recommend.jsx
+++ client/src/Recommend.jsx
@@ -59,6 +59,13 @@ const Recommend = ({ recommendList }) => {
             <span>Save to list</span>
             <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg"><polyline points="6 9 12 15 18 9" /></svg>
           </SaveButton>
+          <Button>
+            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg">
+              <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" />
+              <polyline points="16 6 12 2 8 6" />
+              <line x1="12" y1="2" x2="12" y2="15" />
+            </svg>
+          </Button>
         </RightCTA>
       </CTAContainer>
     );
