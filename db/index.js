9fb0d6d7a9ef0cbec4b91512b7458f6ff419ecf0
Chance Nguyen <chanceknguyen@gmail.com>
2020-10-07 18:39:03 -0500

    Initialized .env throughout service

--- db/index.js
+++ db/index.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-console */
 const mysql = require('mysql');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
 });
 
 connection.connect((err) => {
