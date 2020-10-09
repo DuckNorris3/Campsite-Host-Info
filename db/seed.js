f75a5571b6f5384975b8a06dc7ed6e2c5349ddef
Chance Nguyen <chanceknguyen@gmail.com>
2020-10-09 17:35:48 -0500

    add PW to seed

--- db/seed.js
+++ db/seed.js
@@ -9,6 +9,7 @@ dotenv.config();
 const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
 });
 
 connection.connect((err) => {
